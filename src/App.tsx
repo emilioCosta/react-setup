4e2265e6fceefeb039d2fc6ab043a8cf7cc7f2ca
Emilio de Oliveira Costa <emiliocosta@MacBook-Pro-de-Emilio.local>
2022-04-11 18:48:04 -0300

    React fix husky config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const tempVar = 5;
+import React from 'react'
+import logo from './logo.svg'
 
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Edit and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
